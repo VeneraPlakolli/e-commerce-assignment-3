b7e098f5411a517ff99de6366e431e2c87f5e235
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-06-06 17:21:52 +0200

    tentative error fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-console.log(process.env.NODE_ENV, process.env.ACCESS_DOMAIN);
+console.log(process.env.NODE_ENV, process.env.ACCESS_DOMAIN,'domain');
 
 module.exports = {
   siteMetadata: {
